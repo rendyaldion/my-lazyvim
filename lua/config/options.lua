a64f4bdad23d641edee6d095a4ac169740d14837
rendy <aldionrendy@gmail.com>
2025-02-07 22:00:07 +0700

    feat: add some lib

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options hereby
+
+vim.keymap.set("n", "j", "gj", { noremap = true })
+vim.keymap.set("n", "k", "gk", { noremap = true })
+
+vim.api.nvim_set_keymap("i", "<C-l>", "<Esc>la", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-h>", "<Esc>ha", { noremap = true, silent = true })
